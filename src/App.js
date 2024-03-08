82b7f0d0043ab869dd3fd6bbc1cdd9c4a7d09b23
5nam <ohnam90@gmail.com>
2024-03-08 21:30:51 +0900

    [feat] : 클래스형 컴포넌트 생명주기 학습

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from "react";
 // App 클래스는 React.Component 클래스를 상속받음
 // 클래스형 컴포넌트가 되려면 'App 클래스가 리액트가 제공하는 Component 클래스를 반드시 상속받아야 한다는 것'
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+
   state = {
     count: 0,
   };
@@ -19,7 +24,20 @@ class App extends React.Component {
     }))
   }
 
+  componentDidMount() {
+    console.log('component rendered')
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
