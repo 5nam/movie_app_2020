4bb26d4fc0d3b0bb0c60ad3f626ecd3489bb0a6f
5nam <ohnam90@gmail.com>
2024-03-08 14:55:24 +0900

    [feat] : Potato 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-import Potato from "./Potato";
+
+// 이렿게 짧게 작성할 수 있는 코드는 모두 App 컴포넌트 안에 작성할 것
+function Potato() {
+  return <h1>I like Potato</h1>
+}
 
 function App() {
   // return <div className="App" />;
