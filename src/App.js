24ea026f3858420b46d236e04182375230f4d417
5nam <ohnam90@gmail.com>
2024-03-09 00:29:18 +0900

    [feat] : css 추가

--- src/App.js
+++ src/App.js
@@ -36,15 +36,15 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
     // We are ready 부분에 영화 데이터를 출력할 것
     return (
-      <section>
+      <section className="container">
         {isLoading
         ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
         )
         : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
               <Movie
               key={movie.id}
@@ -53,6 +53,7 @@ class App extends React.Component {
               title={movie.title}
               summary={movie.summary}
               poster={movie.medium_cover_image}
+              genres={movie.genres}
             />
             ))}
           </div>
