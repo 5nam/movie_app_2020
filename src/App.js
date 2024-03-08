0c057aba3487949e5b3cf1112d2beec8032d8c08
5nam <ohnam90@gmail.com>
2024-03-08 20:04:17 +0900

    [feat] : map 함수로 컴포넌트 여러 개 만들기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from "react";
 
 // 이렿게 짧게 작성할 수 있는 코드는 모두 App 컴포넌트 안에 작성할 것
-function Food({ fav }) {
-  console.log(fav);
-  return <h1>I like { fav }</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like { name }</h2>
+      <img src = {picture} />
+    </div>
+  );
 }
+
+// 서버에서 데이터가 넘어온다고 가정하고 데이터 작성
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://www.shutterstock.com/image-photo/korean-kimchi-traditional-cabbage-on-260nw-2252859875.jpg',
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://t3.ftcdn.net/jpg/05/00/83/40/360_F_500834060_MDelPQsZd5hLN7NPm6u5ECadZ3O9l8P2.jpg',
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://i.namu.wiki/i/dgjXU86ae29hDSCza-L0GZlFt3T9lRx1Ug9cKtqWSzMzs7Cd0CN2SzyLFEJcHVFviKcxAlIwxcllT9s2sck0RA.jpg',
+  },
+  {
+    name: 'Doncasu',
+    image: 'https://media.istockphoto.com/id/487660102/ko/%EC%82%AC%EC%A7%84/%EC%9D%BC%EB%B3%B8-%EC%9D%8C%EC%8B%9D-%EB%8F%BC%EC%A7%80%EA%B3%A0%EA%B8%B0-%EC%9D%BC%EC%8B%9D-%EB%8F%88%EA%B9%8C%EC%8A%A4.jpg?s=612x612&w=0&k=20&c=FUfqxVLHtbEt9GIiFcM3P9aYFusHK_zhNOBrjSJMg3M=',
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://previews.123rf.com/images/nontoxicguy/nontoxicguy1704/nontoxicguy170400331/76957445-%EA%B9%80%EB%B0%A5.jpg',
+  },
+]; // foodILike 변수에 빈 배열 저장
 
 function App() {
   // return <div className="App" />;
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" somthing={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi"/>
-
+      {foodILike.map(dish => (<Food name = {dish.name} picture = {dish.image} />))}
     </div>
   );
 }
