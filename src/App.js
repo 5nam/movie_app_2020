b30eae906e6e0f624a7c5a1e33d6701af5bc4c88
5nam <ohnam90@gmail.com>
2024-03-08 16:51:48 +0900

    [feat] : props 이용하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 // 이렿게 짧게 작성할 수 있는 코드는 모두 App 컴포넌트 안에 작성할 것
-function Potato() {
-  return <h1>I like Potato</h1>
+function Food({ fav }) {
+  console.log(fav);
+  return <h1>I like { fav }</h1>;
 }
 
 function App() {
@@ -10,7 +11,11 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" somthing={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi"/>
+
     </div>
   );
 }
