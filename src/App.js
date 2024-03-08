b3890f8bc08f749f2b36d10503acabba3da28f17
5nam <ohnam90@gmail.com>
2024-03-08 21:13:37 +0900

    [feat] : state 도입

--- src/App.js
+++ src/App.js
@@ -1,74 +1,33 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
-// 이렿게 짧게 작성할 수 있는 코드는 모두 App 컴포넌트 안에 작성할 것
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like { name }</h2>
-      <h4>{rating}/5.0</h4>
-      <img src = {picture} alt = {name}/>
-    </div>
-  );
+// App 클래스는 React.Component 클래스를 상속받음
+// 클래스형 컴포넌트가 되려면 'App 클래스가 리액트가 제공하는 Component 클래스를 반드시 상속받아야 한다는 것'
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  }
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-// 서버에서 데이터가 넘어온다고 가정하고 데이터 작성
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://www.shutterstock.com/image-photo/korean-kimchi-traditional-cabbage-on-260nw-2252859875.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://t3.ftcdn.net/jpg/05/00/83/40/360_F_500834060_MDelPQsZd5hLN7NPm6u5ECadZ3O9l8P2.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://i.namu.wiki/i/dgjXU86ae29hDSCza-L0GZlFt3T9lRx1Ug9cKtqWSzMzs7Cd0CN2SzyLFEJcHVFviKcxAlIwxcllT9s2sck0RA.jpg',
-    rating: 4,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://media.istockphoto.com/id/487660102/ko/%EC%82%AC%EC%A7%84/%EC%9D%BC%EB%B3%B8-%EC%9D%8C%EC%8B%9D-%EB%8F%BC%EC%A7%80%EA%B3%A0%EA%B8%B0-%EC%9D%BC%EC%8B%9D-%EB%8F%88%EA%B9%8C%EC%8A%A4.jpg?s=612x612&w=0&k=20&c=FUfqxVLHtbEt9GIiFcM3P9aYFusHK_zhNOBrjSJMg3M=',
-    rating: 3,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://previews.123rf.com/images/nontoxicguy/nontoxicguy1704/nontoxicguy170400331/76957445-%EA%B9%80%EB%B0%A5.jpg',
-    rating: 5,
-  },
-]; // foodILike 변수에 빈 배열 저장
-
-/*
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />;
-}
-// const renderFood = dish => <Food name={dish.name} picture={dish.image} />;
-// 위와 똑같은 기능을 함
-*/
-
-function App() {
-  // return <div className="App" />;
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number, // 
-};
 
 export default App;
